e22fff307e97b2947723a5fbabd9f9eb9166354c
Hunter Ahlquist <contact@hunterahlquist.com>
2021-04-15 09:44:15 -0700

    first

---
+++ pp2/index.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Hunter Ahlquist
+ * 4/15/21
+ * index.php
+ * Pair Program 2
+ */
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Pair Program 2</title>
+</head>
+<body>
+<h1>Pair Program 2</h1>
+<?php
+echo "PHP Array Practice";
+
+
+?>
+</body>
+</html>
+
+
+
